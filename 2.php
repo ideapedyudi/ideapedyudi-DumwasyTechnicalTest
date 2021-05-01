343977f1d706dc9f64457e710620df45bb5f18b4
Muhammad Wahyudi <ideapedyudi@gmail.com>
2021-05-01 15:39:14 +0700

    gambar 2 dan 3

--- 2.php
+++ 2.php
@@ -1,22 +1,9 @@
 <?php
-    $number  = array(1, 4, 8, 9, 3, 12, 0, 7, 6, 2, 11, 10, 5);
+
+    $number  = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
     $string = array("D", "u", "m", "b", "W", "a", "y", "s", "d", "o", "t", "i", "d");
     $jumlah  = count($number);
 
-    $K = 0;
-
-    while ($K < $jumlah - 1) {
-        $i = 0;
-        while ($i < $jumlah - 1 - $K) {
-            if ($number[$i] > $number[$i + 1]) {
-                $X         = $number[$i];
-                $number[$i]     = $number[$i + 1];
-                $number[$i + 1] = $X;
-            }
-            $i++;
-        }
-        $K++;
-    }
     for ($i = 0; $i < $jumlah; $i++)
         echo "$number[$i] ";
 
